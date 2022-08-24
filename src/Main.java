a39345d8fb7cde4f1dfd80f8f93e3659b7f9df71
Yuliya Sidorova <sidorovayuliya883@gmail.com>
2022-08-25 00:11:36 +0300

    count miles

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 10000;
+        int price = 10_000;
         int miles = service.calculate(price);
+        System.out.println("Вам начислено миль:");
         System.out.println(miles);
     }
 }
